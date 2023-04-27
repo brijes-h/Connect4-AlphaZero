531c350afd3fab87f687e10b826b87c8132bcc0d
brijes-h <brijesh_1210@yahoo.com>
2023-04-27 21:00:59 +0530

    update mcts

--- mc_ts.py
+++ mc_ts.py
@@ -1,5 +1,4 @@
 import torch
-# import get_init_board, place_piece, get_valid_moves, is_board_full, is_win
 from game import get_init_board, place_piece, get_valid_moves, is_board_full, is_win
 from view_board import render
 import numpy as np
@@ -13,10 +12,13 @@ board = np.array(
          [-1, 1, 1,-1, 1,-1, 1]] 
          )
 
+# test mcts using dummy model predictions
 def dummy_model_predict(board):
 	value_head = 0.5
 	policy_head = [0.5, 0, 0, 0, 0, 0.5, 0]
 	return value_head, policy_head
+	# policy_head returns action probabilites
+	# value_head tells how strong the board is positioned for a player
 
 class Node:
 	def __init__(self, prior, turn, state):
@@ -26,8 +28,10 @@ class Node:
 		self.children = {}
 		self.value = 0
 
-	def expand(self, action_probs):
-		# [0.5, 0, 0, 0, 0, 0.5, 0]
+	# We try and expand the root node during the MCTS
+	def expand(self, action_probs): 
+		# [0.5, 0, 0, 0, 0, 0.5, 0] -> these are the output values
+		#  (columns that are full have 0s)
 		for action, prob in enumerate(action_probs):
 			if prob > 0:
 				next_state = place_piece(board=self.state, player=self.turn, action=action)
